2518162917152088f66f17a93ec392188d75fc24
David Moreira <davidmoreira37@gmail.com>
2020-10-22 11:36:39 +0100

    Add rename auth hook file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import SignIn from './pages/SignIn';
-import { AuthProvider } from './hooks/AuthContext';
+import { AuthProvider } from './hooks/Auth';
 import ToastContainer from './components/ToastContainer';
 
 import GlobalStyle from './styles/global';
