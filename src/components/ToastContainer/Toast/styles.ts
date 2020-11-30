b61737933a527279fd09fef3cca9d99505c9cf8f
David Moreira <davidmoreira37@gmail.com>
2020-11-30 22:08:35 +0000

    Fix parameter boolean to number passed in component

--- src/components/ToastContainer/Toast/styles.ts
+++ src/components/ToastContainer/Toast/styles.ts
@@ -3,7 +3,7 @@ import { animated } from 'react-spring';
 
 interface ContainerProps {
   type?: 'info' | 'success' | 'error';
-  hasDescription: boolean;
+  $hasDescription: number;
 }
 
 const toastTypeVariations = {
@@ -54,7 +54,7 @@ export const Container = styled(animated.div)<ContainerProps>`
     color: inherit;
   }
   ${props =>
-    !props.hasDescription &&
+    !props.$hasDescription &&
     css`
       align-items: center;
       svg {
