8a832cb0849aa688f55251d37a26cbe4b0742543
David Moreira <davidmoreira37@gmail.com>
2020-12-01 18:06:16 +0000

    Add dashboard next appoitment

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -29,6 +38,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Agenda</h1>
+          <p>
+            <span>Today</span>
+            <span>Day 03</span>
+            <span>Tuesday</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Next appoitment</strong>
+            <div>
+              <img
+                src="https://avatars1.githubusercontent.com/u/64630240?s=60&v=4"
+                alt="test"
+              />
+
+              <strong>Test</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
